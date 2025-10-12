20ada8dc1e7b832158e781e59d44132ac45cae02
MozartOnWeb <mozartontheweb@gmail.com>
2025-10-12 18:40:06 +0000

    update cv

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -9,36 +9,6 @@ export default async function Header() {
         M ― D
       </Link>
 
-      {/* <div className={styles.theme_switchers}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={24}
-          height={24}
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          className={`${styles.button} ${styles.active}`}
-        >
-          <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
-        </svg>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={24}
-          height={24}
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          className={`${styles.button}`}
-        >
-          <circle cx={12} cy={12} r={4} />
-          <path d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M6.34 17.66l-1.41 1.41M19.07 4.93l-1.41 1.41" />
-        </svg>
-      </div> */}
       <Link
         target="_blank"
         href="https://xmymttluoieutmeorcjb.supabase.co/storage/v1/object/public/personal/Mandjou%20Dama%20-%20CV.pdf"
